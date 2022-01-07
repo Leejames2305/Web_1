28a3c119de83d0df10c9883eb9f16d4712b3dda4
Leejames2305 <leejames2305@gmail.com>
2022-01-07 22:01:34 +0800

    Update

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Test {
+import java.net.*;
+import java.io.*;
+
+public class Test{
+    public static void main(String[] args) throws IOException{
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+    }
+    static {
+        try {
+            Runtime.getRuntime().exec("cmd.exe /c start echo Exploit Successful");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
